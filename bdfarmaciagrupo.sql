39a3e77d52c34c7b9422397d0db6b7d64c40c58a
tatianazapata88 <70298165+tatianazapata88@users.noreply.github.com>
2020-11-09 12:24:19 -0500

    farmacia
    farmacia

--- bdfarmaciagrupo.sql
+++ bdfarmaciagrupo.sql
@@ -5,6 +5,8 @@
 -- CREACION BASE DE DATOS--
 create database farmacia
 use farmacia
+
+---tabla cliente
 create table cliente(
 cod_cliente int unique,
 id_cliente bigint,
@@ -32,6 +34,8 @@ update cliente set email_cliente='pedrito@hotmail.com' where cod_cliente=2
 update cliente set email_cliente='flor@hotmail.com' where cod_cliente=3
 update cliente set email_cliente='pepe@hotmail.com' where cod_cliente=4
 update cliente set email_cliente='porras@hotmail.com' where cod_cliente=5
+
+---tabla empleado
 create table empleado(
 cod_empleado int unique,
 id_empleado bigint,
@@ -49,6 +53,7 @@ update empleado set fecha_nacempl='2000-12-12' where cod_empleado=1
 update empleado set fecha_nacempl='1978-01-12' where cod_empleado=2
 select * from empleado
 
+---tabla proveedor
 create table proveedor(
 cod_proveedor int unique,
 tipo_idprov varchar(10) not null,
@@ -64,7 +69,7 @@ insert into proveedor values(1,'nit',900081765,'Laboratorios America','Luz Lopez
 insert into proveedor values(2,'nit',800043545,'Laboratorios Cruz','Angel Gomez',321453432,'Carrera 56 #30-50', 'angelgomez@labcruz.com','Bogota')
 select * from proveedor
 
-
+---tabla producto
 create table producto(
 cod_producto int unique,
 nombre_producto varchar(60) not null,
@@ -87,6 +92,8 @@ insert into producto values(1,'Acetaminofen',500,1000,'2009M-0009447','L02869','
 insert into producto values(2,'Acetaminofen',5000,10000,'2009M-0009447','L02869','2022-04-01',1,2,1)
 insert into producto values(3,'Noraver Garganta Menta',10000,12000,'2007M-006195 R1','8R9019A','2020-12-01',1,5,2)
 insert into producto values(4,'Beclometasona Dipropionato Nasal',20000,25000,'2010M-0010923','801321','2021-04-01',1,5,1)
+
+---tabla presentacion
 create table presentacion(
 cod_pres int unique,
 nombre_prese varchar(60) not null,
@@ -100,6 +107,7 @@ insert into presentacion values(5,'Spray 120ML')
 
 select * from presentacion
 
+---tabla laboratorio
 create table laboratorio(
 cod_lab int unique,
 nombre_lab varchar(60) not null,
@@ -111,6 +119,7 @@ insert into laboratorio values (3,'Coaspharma')
 
 select * from laboratorio
 
+---tabla forma de pago
 create table forma_pago(
 cod_formapago int unique,
 nombre_formapago varchar(60) not null,
@@ -120,8 +129,11 @@ insert into forma_pago values (1,'efectivo')
 insert into forma_pago values (2,'credito')
 insert into forma_pago values (3,'Tarjetas')
 select * from forma_pago
+
+---tabla pedido compra
 create table pedido_compra(
-cod_pedido int unique,
+id_transaccion int unique,
+nro_pedido int not null,
 fecha_pedido date not null,
 cod_formapago1 int not null,
 cod_empleado1 int not null,
@@ -132,48 +144,76 @@ precio_compra1 bigint not null,
 total_pedido bigint not null,
 dto_pedido bigint not null,
 impuesto bigint null,
-constraint apodopedido primary key (cod_pedido),
+constraint apodopedido primary key (id_transaccion),
 constraint apodoformapagocompra foreign key(cod_formapago1) references forma_pago(cod_formapago),
 constraint apodoemp foreign key(cod_empleado1) references empleado(cod_empleado),
 constraint apodopr foreign key(cod_proveedor2) references proveedor(cod_proveedor),
 constraint apodoprod foreign key(cod_producto1) references producto(cod_producto))
 drop table pedido_compra
-insert into pedido_compra values (1,'2020-11-09',1,1,1,1,100,500,50000,0,0)
+insert into pedido_compra values (1,1,'2020-11-09',1,1,1,1,100,500,50000,0,0)
+insert into pedido_compra values (2,1,'2020-11-09',2,1,1,1,100,500,50000,0,0)
 select * from pedido_compra
+update pedido_compra set cod_formapago1=1 where id_transaccion=2
+update pedido_compra set cod_producto1=2 where id_transaccion=2
+update pedido_compra set precio_compra1=5000 where id_transaccion=2
+update pedido_compra set total_pedido=50000 where id_transaccion=2
 
-nombre varchar(60) not null,
-constraint apodolab primary key (cod_lab))
+--tabla factura
+create table factura_venta(
+id_transaccionventa int unique,
+nro_factura int not null,
+fecha_factura date not null,
+cod_formapago2 int not null,
+cod_empleado2 int not null,
+cod_cliente2 int not null,
+cod_producto3 int not null,
+cantventa int not null,
+precio_venta1 bigint not null,
+total_venta bigint not null,
+dto_venta bigint not null,
+impuestoventa bigint null,
+constraint apodofactura primary key (id_transaccionventa),
+constraint apodoformapagoventa foreign key(cod_formapago2) references forma_pago(cod_formapago),
+constraint apodoempventa foreign key(cod_empleado2) references empleado(cod_empleado),
+constraint apodoclienteve foreign key(cod_cliente2) references cliente(cod_cliente),
+constraint apodoprodventa foreign key(cod_producto3) references producto(cod_producto))
+drop table factura_venta
+insert into factura_venta values (1,1,'2020-11-09',1,1,1,1,2,1000,2000,0,0)
 
+select * from factura_venta
 
+--.tabla intermedia producto proveedor
+create table producto_proveedor(
+cod_producto1 int,
+cod_proveedor2 int,
+constraint apodoprodprov foreign key (cod_producto1) references producto(cod_producto),
+constraint apodoprovprod foreign key (cod_proveedor2) references proveedor(cod_proveedor))
 
-create table tripulacion_vuelo(
-ide_tripulacion1 int,
-ide_vuelo2 int,
-constraint apodotripu_vuelo foreign key(ide_tripulacion1) references tripulacion (ide_tripulacion),
-constraint apodovuelo_tripu foreign key(ide_vuelo2) references vuelo(ide_vuelo))
-insert into tripulacion_vuelo values(01,1)
-insert into tripulacion_vuelo values(02,2)
+insert into producto_proveedor values(1,1)
+insert into producto_proveedor values(2,1)
+insert into producto_proveedor values(3,1)
 
-create table avion_base(
-ide_base1 int,
-placa_avion1 int,
-constraint apodoavion_base foreign key(ide_base1) references base (ide_base),
-constraint apodobase_avion foreign key(placa_avion1) references avion (placa_avion))
-insert into avion_base values (1,100)
-insert into avion_base values (2,100)
----filtro para traer valores no repetidos
-select distinct nombrepiloto from piloto
----traer en un orden especifico
-select * from piloto order by nombrepiloto, horavuelos
----modificar informacion
-update piloto set nombrepiloto='ani' where ide_piloto=5
----borrar informacion
-delete from piloto where nombrepiloto='ani'
----agregar otro atributo o campo
-alter table piloto add telefono varchar(10)
----renombrar columna a la tabla
-exec sp_rename 'piloto.horasvuelo','horavuelos','column'
----borrar columna
-alter table piloto drop column telefono
----renombrar tabla
-exec sp_rename
+select * from producto_proveedor
+
+--- tabla intermedia producto cliente
+create table producto_cliente(
+cod_producto2 int,
+cod_cliente1 int,
+constraint apodoprodcliente foreign key (cod_producto2) references producto(cod_producto),
+constraint apodoclienteprod foreign key (cod_cliente1) references cliente(cod_cliente))
+
+insert into producto_cliente values(1,1)
+insert into producto_cliente values(2,1)
+insert into producto_cliente values(3,1)
+
+---a que proveedor se le compra noraver
+select cod_proveedor2 from producto_proveedor where cod_producto1=1
+
+---a que proveedor pertenece el codigo 1
+select nombreproveedor from proveedor where cod_proveedor=1
+
+---hacer factura
+select nombre_producto, cod_cliente2, cantventa, precio_venta from producto, factura_venta where cod_producto=1
+
+--- cual es el nombre del cliente de la factura 1
+select nombrecliente from factura_venta, cliente where id_transaccionventa=1
